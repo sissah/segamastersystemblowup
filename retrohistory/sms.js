c62a149fb9cc33264c215bc00e49db2dd2a94319
sissah <thesissach@gmail.com>
2020-04-24 10:11:40 -0700

    Add files via upload

---
+++ retrohistory/sms.js
@@ -0,0 +1,165 @@
+//arrays for gifs and images
+var playSources = ["images/outrun.gif", "images/sonic2.gif", "images/monsboy3.gif", "images/fzone.gif", "images/space.gif"];
+var boxes = ["images/outbox.jpg", "images/sonicbox.jpg", "images/wb3box.jpg", "images/fz2box.jpg", "images/spacebox.jpg"];
+var devimg = ["images/sg1000.png", "images/sg1000mkii.png", "images/sc-3000.png", "images/smarkIII.png", "images/masterbox.jpg","images/nintendoseal.png", "images/mastergames.jpg", "images/Oldlogo.png"];
+
+var developbox = ["As such the Sega SG-1000 was released in the summer of 1983, an 8-bit video game console comparable to the likes of the Colecovision. While it did see some success in its native region of Japan, it shared its release date with the Famicom, which would go on to dominate the western video game market. The SG-1000 would go through two more iterations, the SG-1000 Mark II (1984) and Mark III respectively.",
+" The Mark II focused on operating as a personal computer, being packaged with programs to teach BASIC and a buffed up CPU (capped at 2.3 MHz v. 1.2 MHz). Alongside this Sega released the SC-3000 that essentially bundled the Mark II’s detachable keyboard with a even more powerful version of the Zilog Z80 CPU, running at 4 MHz.",
+"Where the SC-3000 ultimately failed in the worldwide market, Sega observed the success of the Nintendo Entertainment System and decided diving wholeheartedly into the video game market was the future of the company. Using the CPU used in the SC-3000, and including backwards-compatibility with the SG-1000’s Sega Game Cards, the Sega SG-1000 Mark III was released in October of 1985 to a mediocre response.",
+"Despite being a more powerful console compared to the Famicom, included accessories for personal computer use, and had the entire SG-1000 library of games backing it, it failed to compete with Nintendo’s more affordable console. In response to this, Sega redesigned the Mark III before shipping out to international markets.",
+" Removing the keyboard attachment and any semblance of the home computer aspect, as well reworking the case design for a more distinct, futuristic look, the Mark III had transformed into the Sega Base System and was released in September 1986. The Master System naming was saved for the bundle which included the Light Phazer and the 3-D Glasses, as well as the pack-in games Hang-On and Safari Hunt.",
+"The ‘Master’ branding was essentially an idea drawn from a hat, however it was inspired by the comparison of the video game industry and martial arts, whereas there could be only one “Master” amongst all the competitors. Development of games was a struggle due to Nintendo’s iron grip of a licensing agreement for third party developers.",
+"As Ninendo’s third party partners were not allowed to work with other publishers, Sega’s in house developers were rushed to create games throughout the console’s lifetime, with games having production times of only about three months.",
+"In the early 1980s Sega Enterprises (a merger between Hawaii based coin-op company Sega Corporation and import company Rosen Enterprises) was mainly in the business of producing arcade games. However, as the arcade world began to dwindle in popularity, the company shifted attention to home gaming consoles, as per the advice given by the Hayao Nakayama, president of Sega at the time."];
+
+var legacybox = ["In Brazil, the Sega Master System’s legacy is still prominent. In fact, as late as 2012 there were an estimated 5 million copies of the system were on the market, dwarfing even the 3 million copies released of the Sega Genesis during its lifespan in North America (Brazil is an Alternate Video Game Universe). As of 2015, the system was still selling 150,000 units a year. Sega’s marketing in the under-served country resulted in a large and loyal fanbase that enjoys the system to this day (Big in Brazil).",
+"The Sega Master System was a pivotal point in time for Sega. During this period, Sega saw two mascots developed. While Sonic the Hedgehog may be the first thought made by American audiences when they they think “Sega” and “mascot,” but long before Sonic’s arrival on both the Genesis and Master System, the established mascot for Sega was a monkey-child named Alex Kidd. In fact, many copies of the Master Drive II were release with a copy of Alex Kidd in Miracle World bundled with the console, making it the first game many players got for the system. Outside of the US, the character is sometimes regarded with the same recognition and nostalgia, if not more, than Nintendo’s mascot Mario.",
+"After a rough start in the home console industry, Sega managed to find its home in markets untouched by Nintendo. Learning from their mistakes and leveraging their success in these other markets, Sega was able to pave the way for their next console, the Sega Genesis, and put an end to what may nearly have been a Nintendo monopoly of the industry. While eventually Nintendo would win out against Sega, the competition left room for other competitors to step in. The fierce competitiveness between companies lead to the established “generations” of consoles as we see them today. The very notion of “console wars,” which has become so iconic of the game industry, would have happened if not for the Sega Master Drive (Kohler).",
+"Nintendo already having a strong hold of the market in North America and Japan, the Sega Master System found greater success in the European and Brazilian markets. Even so, the dominance of Nintendo and with Sega as a distant runner up in the wake of Atari’s downfall, marked a shift away from an industry led by American game companies towards Japanese game companies. However, with greater marketing, a lower cost than Nintendo’s console, and a larger collection of games than were released in North America, the Sega Master System was the popular system of the time in Europe, selling nearly 1.75 million consoles in 1991 alone (Sega Retro)."];
+
+var impactbox = ["At the time, Nintendo had left the campaigning up to Mattel who basically tried to sweep the console under the carpet as much as possible. Not to mention the high price for an NES and its games also played a factor as it was just not affordable compared to competing systems at the time.",
+"After gathering experience from their failure in the U.S. and Japanese markets, Sega studied from Nintendo’s mistakes and handed the distribution rights to Master Games in France and Mastertronic who were both versed in the UK gaming scene with their Spectrum background.",
+"They were able to adjust the price for the Master System and make it lower than the NES by about 40-60 euros, with a sub 100 euros price point and cheaper games. They advertised the system’s appeal as being an “arcade in the home”, capitalizing on the instant loading times and better graphics resolution and animation than the rest of the competition.",
+"The advertising garnered the attention of the gaming press and received high retail orders from the get go. So high that Sega was unable to deliver any stock until Boxing Day. This could have been a good business opportunity for Nintendo to put an end to the Master System as Mastertronic and Master Games were financially vulnerable around this time.",
+"However, they were saved when Virgin, keen to grab a piece of the Sega pie, bought out Mastertronic and later in 1988, took over all European distribution. In January of 1988, systems were able to be stocked on store shelves in time, and many families were introduced to the wonders of instantaneous loading, something of a marvel in a country rooted firmly in tape loaded software.",
+"Worldwide sales for the Master System are approximated to be 13 million, a small fraction of the NES’s 60 million. Broken down though, the Master System sold just 1.5 million in North America, and 10 million in other regions excluding Japan. This actually tops the 8.5 million NES systems sold to the same regions, with many of those sales actually coming early on, the rivalry with Nintendo would be further closed by the Mega Drive’s 40 million sales to 49 million of the SNES.",
+"Brazil is not a dominant gaming market in the world stage, but where the Master System is concerned, it experienced quite a bit of success. Released in 1989, the Master System was handed over to local toy manufacturer TecToy for distribution, who released a number of bundles, labeling the release of the Alex Kidd Bundle as the Master System II. This meant that when the actual Master System II hardware was released, the Brazilian version was marketed as the Master System III Compact.",
+"Various other releases were created including the Master System Girl and Master System Super Compact which allows wireless RF transmission. Total Master System sales before the end of the 20th century was approximately 6 million in this region, leading to a total worldwide figure of about 19 million.",
+"TecToy continued developing a number of well implemented region exclusive titles up until 1998 such as Street Fighter 2 and Earthworm Jim. In 2015, it was reported that the Master System still sold around 150,000 units per year in Brazil. It incorporates 132 games, although development of any new games have stopped for a long time by now, the last cartridge based unit was manufactured in 2003. Since 2000 about 2 million Master System based units have been sold in this area.",
+"When the Master System came out, it was already a technically good system for its time, and was able to enjoy most of its success in Europe from 1987 to 1996 after taking the opportunity to reach towards the European market after seeing that Nintendo has been neglecting that particular region. Nintendo’s marketing was not able to capture the general audience in Europe at the release of the NES in the mid-80s due to improper market management."];
+
+var devcount = 0;
+var legcount = 0;
+var impcount = 0;
+var statiky = document.querySelector('.static');
+statiky.style.visibility = 'hidden';
+
+//var sound = document.getElementById("sboot");
+
+//play audio at boot (not working)
+function playSound(){
+  sound.play();
+}
+
+//swap through gameplay gifs
+function swapGame(){
+      var index = 0;
+      setInterval(function(){
+        if (index === playSources.length) {
+          index = 0;
+        }
+        document.getElementById("gameplay").src = playSources[index];
+        index++;
+      }, 5000);
+}
+//swap through box art
+function swapBox(){
+      var indexe = 0;
+      setInterval(function(){
+        if (indexe === boxes.length) {
+          indexe = 0;
+        }
+        document.getElementById("gamebox").src = boxes[indexe];
+        indexe++;
+        staticShift();
+      }, 5000);
+      //staticShift();
+}
+/*Hide static function*/
+function staticShift() {
+  setTimeout(function(){
+    statiky.style.visibility = 'hidden';
+  },500);
+  statiky.style.visibility = 'visible';
+}
+
+//calls function at page load
+window.addEventListener('load', function (){
+    swapGame();
+    swapBox();
+
+});
+
+//Functions that call css to stop scrolling
+function stopScroll() {
+  document.body.classList.add("stop-scrolling");
+}
+function scrollOn() {
+  document.body.classList.remove("stop-scrolling");
+}
+function changeDevText() {
+  if(devcount >= 7 ) {
+    document.getElementById('developtext').innerHTML = developbox[7];
+    document.getElementById('devpics').src = devimg[7];
+    devcount = 0;
+  }
+  else {
+    document.getElementById('developtext').innerHTML = developbox[devcount];
+    document.getElementById('devpics').src = devimg[devcount];
+    devcount++;
+  }
+}
+function changeLegText() {
+  if(legcount >= 3 ) {
+    document.getElementById('legacytext').innerHTML = legacybox[3];
+    legcount = 0;
+  }
+  else {
+    document.getElementById('legacytext').innerHTML = legacybox[legcount];
+    legcount++;
+  }
+}
+function changeImpText() {
+  if(impcount >= 9 ) {
+    document.getElementById('impacttext').innerHTML = impactbox[9];
+    impcount = 0;
+  }
+  else {
+    document.getElementById('impacttext').innerHTML = impactbox[impcount];
+    impcount++;
+  }
+}
+
+//open the Sega Master System diagram
+function openSMS(){
+  // Code for IE9
+  document.getElementById("topsms").style.msTransform = "translateY(50px)";
+  // Standard syntax
+  document.getElementById("topsms").style.transform = "translateY(50px)";
+  document.getElementById("shellsms").style.msTransform = "translateY(100px)";
+  document.getElementById("shellsms").style.transform = "translateY(100px)";
+  document.getElementById("boardsms").style.msTransform = "translateY(150px)";
+  document.getElementById("boardsms").style.transform = "translateY(150px)";
+  document.getElementById("basesms").style.msTransform = "translateY(200px)";
+  document.getElementById("basesms").style.transform = "translateY(200px)";
+}
+//close the Sega Master System diagram
+function closeSMS(){
+  // Code for IE9
+  document.getElementById("topsms").style.msTransform = "translateY(0px)";
+  // Standard syntax
+  document.getElementById("topsms").style.transform = "translateY(0px)";
+  document.getElementById("shellsms").style.msTransform = "translateY(0px)";
+  document.getElementById("shellsms").style.transform = "translateY(0px)";
+  document.getElementById("boardsms").style.msTransform = "translateY(0px)";
+  document.getElementById("boardsms").style.transform = "translateY(0px)";
+  document.getElementById("basesms").style.msTransform = "translateY(0px)";
+  document.getElementById("basesms").style.transform = "translateY(0px)";
+}
+/*//onload function, start functions immediately when page loads. Functions
+//don't start simultaneous so won't be using it now. Code by Simon Willison
+function addLoadEvent(func) {
+  var oldonload = window.onload;
+  if (typeof window.onload != 'function') {
+    window.onload = func;
+  } else {
+    window.onload = function() {
+      if (oldonload) {
+        oldonload();
+      }
+      func();
+    }
+  }
+}
+addLoadEvent(swapGame);
+addLoadEvent(swapBox);*/
